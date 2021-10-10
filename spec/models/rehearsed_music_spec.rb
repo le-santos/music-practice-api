98001a413b4e478e5bcb4a81cebf2450a5874650
le-santos <lequinterio@gmail.com>
2021-10-10 13:15:44 -0300

    test: add validations for rehearsed_music

--- spec/models/rehearsed_music_spec.rb
+++ spec/models/rehearsed_music_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe RehearsedMusic, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { is_expected.to belong_to(:practice_session) }
+    it { is_expected.to belong_to(:music) }
+  end
 end
