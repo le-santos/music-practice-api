cdcde79b77bae7bf27aa9e450744a09ccb72770b
le-santos <lequinterio@gmail.com>
2021-10-10 13:12:11 -0300

    test: validations for practice_session

--- spec/factories/rehearsed_musics.rb
+++ spec/factories/rehearsed_musics.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :rehearsed_music do
-    practice_session { PracticeSession.last || association(:practice_session) }
-    music { Music.last || association(:music) }
+    practice_session { association(:practice_session) }
+    music { association(:music) }
   end
 end
--- spec/models/practice_session_spec.rb
+++ spec/models/practice_session_spec.rb
@@ -2,29 +2,32 @@ require 'rails_helper'
 
 RSpec.describe PracticeSession, type: :model do
   context 'validation' do
-    # add tests
+    subject { create(:practice_session) }
+
+    it { is_expected.to validate_presence_of(:goals) }
   end
 
   context 'associations' do
-    it 'has many rehearsed_music' do
-      practice_session = FactoryBot.create(:practice_session)
-      music1 = FactoryBot.create(:music)
-      music2 = FactoryBot.create(:music)
-      RehearsedMusic.create!(practice_session: practice_session, music: music1)
-      RehearsedMusic.create!(practice_session: practice_session, music: music2)
+    let(:music1) { create(:music) }
+    let(:music2) { create(:music) }
+    let(:practice_session) { create(:practice_session) }
+    let(:rehearsed_musics) do
+      create(:rehearsed_music,
+             practice_session: practice_session,
+             music: music1)
+      create(:rehearsed_music,
+             practice_session: practice_session,
+             music: music2)
+    end
 
+    before { rehearsed_musics }
+
+    it 'has many rehearsed_music' do
       expect(practice_session.rehearsed_musics.count).to eq 2
       expect(practice_session.rehearsed_musics.first.id).to eq music1.id
-      expect(practice_session.rehearsed_musics.last.id).to eq music2.id
     end
 
     it 'has many musics through rehearsed_musics' do
-      practice_session = FactoryBot.create(:practice_session)
-      music1 = FactoryBot.create(:music)
-      music2 = FactoryBot.create(:music)
-      RehearsedMusic.create!(practice_session: practice_session, music: music1)
-      RehearsedMusic.create!(practice_session: practice_session, music: music2)
-
       expect(practice_session.musics.count).to eq 2
       expect(practice_session.musics.first.id).to eq music1.id
       expect(practice_session.musics.last.id).to eq music2.id
