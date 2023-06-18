50576be05309fb93d4d53235b5d80176de0ed556
le-santos <lequinterio@gmail.com>
2023-06-18 18:40:52 -0300

    feat: add method to music model

--- app/models/music.rb
+++ app/models/music.rb
@@ -6,4 +6,8 @@ class Music < ApplicationRecord
   has_many :practice_sessions, dependent: :destroy
 
   enum status: { archived: 0, learning: 1, reviewing: 2, performing: 9 }
+
+  def last_played_at
+    practice_sessions.order(created_at: :desc).first&.created_at
+  end
 end
--- spec/models/music_spec.rb
+++ spec/models/music_spec.rb
@@ -28,4 +28,23 @@ RSpec.describe Music, type: :model do
       expect(music.user).to eq user
     end
   end
+
+  context '#last_played_at' do
+    let(:music) { create(:music) }
+    let(:older_practice_session) { create(:practice_session, created_at: 3.days.ago, music:) }
+    let(:newer_practice_session) { create(:practice_session, created_at: 1.days.ago, music:) }
+
+    it 'returns last_played date based on the recent PracticeSession' do
+      older_practice_session
+      newer_practice_session
+
+      expect(music.last_played_at).to eq(newer_practice_session.created_at)
+    end
+
+    context 'when music does not have any practice_session' do
+      it 'returns nil' do
+        expect(music.last_played_at).to be_nil
+      end
+    end
+  end
 end
