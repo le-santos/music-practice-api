d79a51365a0328f30bce5103f560089bfc4d371c
le-santos <lequinterio@gmail.com>
2026-01-10 21:15:11 -0300

    Refactor user validations and update locale for practice sessions; adjust Devise sign-out method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Obs: some validations are already given by devise's validatable module
   # https://github.com/heartcombo/devise/blob/main/lib/devise/models/validatable.rb
 
-  validates :username, :email, :password, presence: true, allow_blank: false
+  validates :username, :email, presence: true, allow_blank: false
   validates :username, :email, uniqueness: { case_sensitive: true }
 
   has_many :musics, dependent: :destroy
